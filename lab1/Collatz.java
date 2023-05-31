a38afa74e2ea40f8b4c8f43c09b50679286425cd
JUSTLOVELE <694105388@qq.com>
2023-05-31 14:42:51 +0800

    add

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,16 +2,27 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-
+    //如果 n 为偶数，则下一个数字为 n/2。如果 n 为奇数，则下一个数字为 3n + 1。如果 n 为 1，则序列结束。
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+
+        if(n == 1) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        } else if(n%2 == 0) {
+            return n/2;
+        }else if(n % 2 != 0) {
+            return 3*n + 1;
+        }else {
+            return -1;
         }
+
+//        if (n  == 128) {
+//            return 1;
+//        } else if (n == 5) {
+//            return 3 * n + 1;
+//        } else {
+//            return n * 2;
+//        }
     }
 
     public static void main(String[] args) {
