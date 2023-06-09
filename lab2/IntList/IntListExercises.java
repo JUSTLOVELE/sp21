8edc698b608ff230a01483eec1214b43d3576e3e
JUSTLOVELE <694105388@qq.com>
2023-06-10 00:30:56 +0800

    add

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,7 +10,7 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
@@ -51,6 +51,11 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
+
+        if(lastDigit == 0) {
+            return false;
+        }
+
         while (x > 10) {
             x = x / 10;
         }
