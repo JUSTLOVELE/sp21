f1dfd4eac2cc0ff020692d286f87ab7bb13e781a
JUSTLOVELE <694105388@qq.com>
2023-09-26 16:41:34 +0800

    Update Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -6,23 +6,23 @@ public class Collatz {
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
 
-        if(n == 1) {
-            return 1;
-        } else if(n%2 == 0) {
-            return n/2;
-        }else if(n % 2 != 0) {
-            return 3*n + 1;
-        }else {
-            return -1;
-        }
-
-//        if (n  == 128) {
+//        if(n == 1) {
 //            return 1;
-//        } else if (n == 5) {
-//            return 3 * n + 1;
-//        } else {
-//            return n * 2;
+//        } else if(n%2 == 0) {
+//            return n/2;
+//        }else if(n % 2 != 0) {
+//            return 3*n + 1;
+//        }else {
+//            return -1;
 //        }
+
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
     }
 
     public static void main(String[] args) {
